3c8f2c2cad88f883a56993ded057649200340c5b
Alfonsodoamaral <alfonsodoamaral@gmail.com>
2025-11-09 14:58:01 -0300

    hicimos home likes y post

---
+++ src/components/Post.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View, Text, Pressable } from 'react-native';
+import { db } from '../firebase/config';
+import { auth } from '../firebase/config';
+import firebase from 'firebase/app';
+
+class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            post: [],
+            like: false,
+
+        }
+    }
+    componentDidMount() {
+        console.log(this.props)
+        if (this.props.data.likes.includes(auth.currentUser.email)) {
+            this.setState({ like: true })
+        } 
+
+    }
+    likear() {
+        if (this.props.data.likes.includes(auth.currentUser.email)) {
+            db.collection('posts').doc(this.props.id).update({
+                likes: firebase.firestore.FieldValue.arrayRemove(auth.currentUser.email)
+
+            })
+                .then(() => {
+                    this.setState({ like: false })
+                })
+        } else {
+            db.collection('posts').doc(this.props.id).update({
+                likes: firebase.firestore.FieldValue.arrayUnion(auth.currentUser.email)
+            })
+                .then(() => {
+                    this.setState({ like: true })
+                })
+        }
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>{this.props.data.texto}</Text>
+                <Text>{this.props.data.owner}</Text>
+                <Text>{this.props.data.likes.length}</Text>
+                <Pressable onPress={() => this.likear()}>
+                    <Text>{this.state.like ? "Sacar like" : "likear"}</Text>
+                </Pressable>
+
+            </View>
+        )
+    }
+}
+export default Post;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,38 @@
-import { Text, View } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
 import React, { Component } from 'react'
+import { db } from '../firebase/config';
+import Post from '../components/Post';
 
 export class Home extends Component {
     constructor(props) {
         super(props);  
         this.state = {
+          posts:[]
 
         }
+    }
+    componentDidMount() {
+       db.collection('posts').orderBy('createdAt', 'desc').onSnapshot(docs => {
+        let posts = [];
+        docs.forEach(doc => {
+          posts.push({
+            id: doc.id,
+            data: doc.data()
+        
+          });
+
+        }); 
+        console.log(posts);
+        this.setState({
+          posts: posts
+        });
+       });
     }
   render() {
     return (
       <View>
         <Text>Home</Text>
+        <FlatList data={this.state.posts} keyExtractor={(item) => item.id} renderItem={({item}) => <Post data={item.data} id={item.id}/>}/>
       </View>
     )
   }
